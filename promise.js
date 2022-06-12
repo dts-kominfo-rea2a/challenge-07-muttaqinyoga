79393abaa8330c0e3c10c7160becb308d9d49ed1
muttaqinyoga <muttaqinyoga@gmail.com>
2022-06-12 23:46:53 +0700

    Menyelesaikan tugas yang diberikan

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (reactions) => {
+  try{
+    const resultsReaction = (await promiseTheaterIXX()).concat((await promiseTheaterVGC()));
+    return resultsReaction.filter(res => (res.hasil === reactions)).length;
+  } catch(err){
+    console.log(err);
+  }
+}
 
 module.exports = {
   promiseOutput,
